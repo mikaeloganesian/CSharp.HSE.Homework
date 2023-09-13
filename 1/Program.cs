92e19c87cf378932be5e9282499266068ded8818
mikaeloganesian <mikaeloganesian2005@mail.ru>
2023-09-13 22:50:52 +0300

    1-2 Семинары

--- 1/Program.cs
+++ 1/Program.cs
@@ -63,6 +63,9 @@ class Program
         Console.WriteLine(string.Format("Сумма дробных частей чисел {0} и {1} равна {2:N2}", firstNum, secondNum, firstNum - (int)firstNum + secondNum - (int)secondNum));
         */
 
+
+        //Семинар 2, Self, Self03
+        /*
         int x = int.Parse(Console.ReadLine());
         int y = int.Parse(Console.ReadLine());
         int z = int.Parse(Console.ReadLine());
@@ -71,8 +74,51 @@ class Program
         minVal = x < y ? (x < z ? x : z) : (y < z ? y : z);
         avVal = x + y + z - maxVal - minVal;
         Console.WriteLine(minVal + " " + avVal + " " + maxVal);
+        */
+
+
+        //Семинар 2, Self, Self04, Solution№1
+        /*
+        char s = Convert.ToChar(Console.ReadLine());
+        string output = char.IsDigit(s) == true ? "Это цифра" : char.IsLetter(s) == true ? "Это буква" : "Это ни буква, ни цифра";
+        Console.WriteLine(output);
+        */
+
+
+        //Семинар 2, Self, Self04, Solution№2
+        /*
+        char s = Convert.ToChar(Console.ReadLine());
+        char[] alf = {  'А', 'а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Д', 'д', 'Е', 'е', 'Ё', 'ё', 'Ж', 'ж', 'З', 'з',
+                        'И', 'и', 'Й', 'й', 'К', 'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'Р', 'р',
+                        'С', 'с', 'Т', 'т', 'У', 'у', 'Ф', 'ф', 'Х', 'х', 'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ',
+                        'Ъ', 'ъ', 'Ы', 'ы', 'Ь', 'ь', 'Э', 'э', 'Ю', 'ю', 'Я', 'я' };
+
+        string output = char.IsDigit(s) == true ? "Это цифра" : alf.Contains(s) == true ? "Это буква русского алфавита" : "Это ни буква русского алфавита, ни цифра";
+        Console.WriteLine(output);
+        */
+
+
+        //Семинар 2, Self, Self05
+        /*
+        double R = double.Parse(Console.ReadLine());
+        int x = int.Parse(Console.ReadLine()), y = int.Parse(Console.ReadLine());
+        string output = x * x + y * y <= R ? "Точка внутри круга!" : "Точка вне круга!";
+        Console.WriteLine(output);
+        */
+
+
+        //Семинар 2, Self, Self06
+        /*
+        string str = Console.ReadLine();
+        string s = int.Parse(str) >= 0 ? str : Convert.ToString(int.Parse(str) * -1);
+        for (int i = 0; i < s.Length; i++)
+        {
+            Console.WriteLine(s[i]);
+        }
 
         Console.ReadKey();
+        */
+        ///Примечание: для тестирования раскомментировать конкретную задачу
     }
 
 }
